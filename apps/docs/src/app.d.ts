8bc55021e9e848c43a2f75909f4a31f83511c57b
josmas <josmasflores@gmail.com>
2026-08-31 18:36:12 +0100

    add License file and copyright in each file

--- apps/docs/src/app.d.ts
+++ apps/docs/src/app.d.ts
@@ -1,3 +1,7 @@
+// Copyright 2026 appinventor.org
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
--- apps/docs/src/lib/index.ts
+++ apps/docs/src/lib/index.ts
@@ -1,1 +1,5 @@
+// Copyright 2026 appinventor.org
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 // place files you want to import through the `$lib` alias in this folder.
